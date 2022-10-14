3f2c1e725c22cd252dbf53f2e2c5385f539f5e0f
Szymon Rogowski <szymon0r0official@gmail.com>
2022-10-14 12:05:43 +0200

    Loading files from UPEL lab

---
+++ main.cpp
@@ -0,0 +1,64 @@
+/*********************************************
+Kod stanowi uzupe³nienie materia³ów do æwiczeñ
+w ramach przedmiotu metody optymalizacji.
+Kod udostêpniony na licencji CC BY-SA 3.0
+Autor: dr in¿. £ukasz Sztangret
+Katedra Informatyki Stosowanej i Modelowania
+Akademia Górniczo-Hutnicza
+*********************************************/
+
+#include"opt_alg.h"
+
+void lab1();
+void lab2();
+void lab3();
+void lab4();
+void lab5();
+void lab6();
+
+int main()
+{
+    cout << "dzialam1\n";
+	try
+	{
+
+	}
+	catch (string EX_INFO)
+	{
+		cerr << "ERROR:\n";
+		cerr << EX_INFO << endl << endl;
+	}
+    cout << "dzialam2\n";
+	system("pause");
+	return 0;
+}
+
+void lab1()
+{
+
+}
+
+void lab2()
+{
+
+}
+
+void lab3()
+{
+
+}
+
+void lab4()
+{
+
+}
+
+void lab5()
+{
+
+}
+
+void lab6()
+{
+
+}
